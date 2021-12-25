3b186851572d0b4a015ef6eba40c9f1a94a54ff2
sheraram <sheraram998202@gmail.com>
2021-12-25 23:01:45 +0530

    till video 8 complete

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+      
+      <Navbar title="My App"/>
+      {/* <Navbar/> */}
+
+      <div className="container my-3">
+        <TextForm heading="Enter the text for analysis below"/>
+      </div>
+      
+
+    </>
+  );
+}
+
+export default App;
